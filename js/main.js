564cfff4bb6661c0e419a1daeacd3fa208725902
Zachary Mays <zackmays@gmail.com>
2015-07-31 14:47:41 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $("#submit").on("click", function(e) {
+        e.preventDefault();
+        decode_url();
+        $("#result-container").slideDown(200);
+    })
+})
+
+function decode_url()
+{
+    var url = document.getElementById("url").value;
+    var resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = null;
+    
+    if ( url.indexOf("?") >= 0 ) {
+        var urlParts = url.split("?");
+        var params = urlParts[1];
+        var paramsList = params.split("&");
+        
+        paramsList.forEach(function(item) {
+            var decodedItem = item.replace(/%20/g, ' ').replace(/%2F/g, '/');
+            keyValue = decodedItem.split("=");
+            if ( !keyValue[1] ) {
+                keyValue[1] = '';
+            }
+            resultContainer.innerHTML += "<p>" + keyValue[0] + " => " + keyValue[1] + "</p>";
+        });
+    } else {
+        resultContainer.innerHTML += "<p><em>Invalid URL or no parameters passed.</em></p>"
+    }
+    
+    return false;
+}
+
+function reload_page()
+{
+    document.getElementById("form").reset();
+    window.location.reload();
+}
