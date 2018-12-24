b86d741b5431f14b6b08d70d572226b790a06717
Zachary Mays <zackmays@gmail.com>
2018-12-24 08:54:14 -0500

    Updates replace method with function for easy maintanence

--- js/main.js
+++ js/main.js
@@ -11,30 +11,55 @@ function decode_url()
     var url = document.getElementById("url").value;
     var resultContainer = document.getElementById("result");
     resultContainer.innerHTML = null;
-    
-    if ( url.indexOf("?") >= 0 ) {
-        var urlParts = url.split("?");
-        console.log(urlParts.length);
+
+    var params = null;
+
+    if (url.indexOf("?") >= 0) {
+        urlParts = url.split("?");
         if ( urlParts.length > 2 ) {
             resultContainer.innerHTML += "<p class='text-danger'><em>Invalid URL - too many query separators (?).</em></p>"
             return false;
         }
         var params = urlParts[1];
-        var paramsList = params.split("&");
-        
-        paramsList.forEach(function(item) {
-            var decodedItem = item.replace(/%20/g, ' ').replace(/%2F/g, '/').replace(/%40/g, '@').replace(/%3A/g, ':').replace(/%5B/g, '[').replace(/%5D/g, ']').replace(/%2C/g, ',').replace(/\+/g, ' ');
-            keyValue = decodedItem.split("=");
-            if ( !keyValue[1] ) {
-                keyValue[1] = '';
-            }
-            resultContainer.innerHTML += "<p>" + keyValue[0] + " => " + keyValue[1].replace(/%3D/g, '=') + "</p>";
-        });
     } else {
-        resultContainer.innerHTML += "<p class='text-danger'><em>Invalid URL or no parameters passed.</em></p>"
+        params = url;
     }
+
+    var paramsList = params.split("&");
+
+    paramsList.forEach(function(item) {
+        var decodedItem = decode_url_param(item);
+        keyValue = decodedItem.split("=");
+        if ( !keyValue[1] ) {
+            keyValue[1] = '';
+        }
+        resultContainer.innerHTML += "<p>" + keyValue[0] + " => " + keyValue[1].replace(/%3D/g, '=') + "</p>";
+    });
     
     return false;
+}
+
+function decode_url_param(param)
+{
+    var decodeList = {
+        "%20": " ",
+        "%2F": "/",
+        "%40": "@",
+        "%3A": ":",
+        "%5B": "[",
+        "%5D": "]",
+        "%2C": ",",
+        "%7B": "{",
+        "%7D": "}",
+        "%22": "\"",
+    };
+
+    for (var k in decodeList){
+        var expression = new RegExp(k, "g");
+        param = param.replace(expression, decodeList[k]);
+    }
+
+    return param;
 }
 
 function reload_page()
